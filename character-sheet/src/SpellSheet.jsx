424578ce08dfc293e80c69557353a305813f8502
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2023-06-25 13:52:22 -0400

    remove imports

--- character-sheet/src/SpellSheet.jsx
+++ character-sheet/src/SpellSheet.jsx
@@ -1,11 +1,8 @@
 import SpellClass from './SpellClass'
-import SpellHeaderRow from './SpellHeaderRow'
 import SpellLevelDividerRow from './SpellLevelDividerRow'
 import SpellList from './SpellList'
-import SpellRow from './SpellRow'
 import './SpellSheet.css'
 import SpellSlots from './SpellSlots'
-import spellListColumns from './spellListColumns'
 
 export default function SpellSheet() {
   return (
